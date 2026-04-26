b191d2a8a0348c65f56b5478f3b7dfc3095a1ddd
likeorangeovo <845156436@qq.com>
2026-04-27 00:08:57 +0800

    RMSNorm

--- model/model.py
+++ model/model.py
@@ -70,3 +70,16 @@ class MokioMindConfig(PretrainedConfig):
             if self.inference_rope_scaling
             else None
         )
+
+
+class RMSNorm(nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-5):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        return self.weight * self._norm(x.float()).type_as(x)
